84573ce8c6c081bfb5df176fda8ce26b534741f2
epoli-Yang <3190104702yzj@gmail.com>
2026-03-29 03:52:33 +0800

    Delete Ocean/Ocean directory

--- Ocean/Ocean/src/services/supabase.js
+++
@@ -1,4 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-import { SUPABASE_CONFIG } from '@/config/api'
-
-export const supabase = createClient(SUPABASE_CONFIG.URL, SUPABASE_CONFIG.ANON_KEY)
